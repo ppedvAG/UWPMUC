e94ee7df7f5a602e0460b13f919af7c7483b8da3
Hannes Preishuber <hannesp@ppedv.de>
2019-06-05 15:04:48 +0200

    PopUp Dialoge

---
+++ UWPMUC/Modul06/BlankPage3.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace UWPMUC.Modul06
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class BlankPage3 : Page
+    {
+        public BlankPage3()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            var dialog = new ContentDialog();
+            dialog.Content = new TextBlock() { Text = "Hallo Welt" };
+            dialog.Title = "Titel";
+            dialog.PrimaryButtonText = "OK";
+            dialog.SecondaryButtonText = "Cancel";
+            dialog.IsSecondaryButtonEnabled = true;
+            dialog.CloseButtonText = "close";
+            if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+            {
+                var msg = new MessageDialog("Sind sie sicher?", "ok?");
+
+                msg.ShowAsync();
+            }
+
+        }
+    }
+}
