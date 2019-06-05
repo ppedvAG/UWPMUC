2fe5987ab015656d0233a0ce32ed63318f3b8455
Hannes Preishuber <hannesp@ppedv.de>
2019-06-05 09:39:06 +0200

    Farbverlauf Slider

---
+++ UWPMUC/Modul05/BlankPage1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace UWPMUC.Modul05
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+            //rect1.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+        }
+
+        private void Slider1_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            gradient1.Offset = slider1.Value;
+        }
+    }
+}
