3f0c4e90a4f7f6e0803f43da96035a4469c74a54
Hannes Preishuber <hannesp@ppedv.de>
2019-06-06 14:29:40 +0200

    MVVM Icommand

---
+++ UWPMUC/Modul10/BlankPage1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPMUC.Modul06;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace UWPMUC.Modul10
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public ToDo MyToDo { get; set; }
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+            MyToDo = new ToDo();
+            MyToDo.Id = 1;
+            this.DataContext = MyToDo;
+        }
+    }
+}
---
+++ UWPMUC/Modul10/ToDoVM.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UWPMUC.Modul06;
+
+namespace UWPMUC.Modul10
+{
+    class ToDoVM
+    {
+        public ToDoVM()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                ID = 100;
+            }
+            
+        }
+        public int ID { get; set; }
+
+        private ToDo _NeuToDo;
+
+        public ToDo NeuToDo
+        {
+            get { return _NeuToDo; }
+            set { _NeuToDo = value; }
+        }
+        
+        public ICommand SaveNeuCommand => new DelegateCommand(SaveNeuTodo);
+        public void SaveNeuTodo()
+        {
+
+        }
+
+    }
+
+    internal class DelegateCommand : ICommand
+    {
+        private Action _mycommand;
+
+        public DelegateCommand(Action mycommand)
+        {
+            this._mycommand = mycommand;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _mycommand();
+        }
+    }
+}
