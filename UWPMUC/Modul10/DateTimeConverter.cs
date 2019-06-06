3dad17bf0a441d0eb95276ed92b99bffd0f69b9c
Hannes Preishuber <hannesp@ppedv.de>
2019-06-06 16:14:14 +0200

    Compiled Binding COnverter

---
+++ UWPMUC/Modul10/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWPMUC.Modul10
+{
+    class DateTimeConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+          
+            return DateTimeOffset.Now;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+          
+
+            return DateTime.Parse(value.ToString());
+
+        }
+    }
+}
